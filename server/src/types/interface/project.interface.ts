a374becb2a656f99ad9aa3455cb51ab90964f3d7
sankar-raul <raulsankar99@gmail.com>
2026-03-03 22:47:05 +0530

    feat: implement project service with CRUD operations and member management

--- server/src/types/interface/project.interface.ts
+++ server/src/types/interface/project.interface.ts
@@ -1,9 +1,5 @@
-import { Types } from "mongoose";
-import { ROLE } from "@/constants/role.constant";
-export interface IProjectMember {
-  user: Types.ObjectId;
-  role: typeof ROLE[keyof typeof ROLE];
-}
+import { IProjectMember } from "./projectMember.interface";
+
 export interface IProject {
   projectName: string;
   description?: string;
