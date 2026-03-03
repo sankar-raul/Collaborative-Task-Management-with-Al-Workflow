2aee5413f00ea8fd75862c14d935ad409284a05b
sankar-raul <raulsankar99@gmail.com>
2026-03-03 18:30:16 +0530

    chore: remove unnecessary blank line in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -28,7 +28,6 @@ export const io = new Server(server, {
   cors: { origin: "*" }
 });
 
-
 io.use(async (socket, next) => {
     // const token = cookie.parse(socket.request.headers.cookie || '')?.access_token
     const token = socket.handshake.auth.token
