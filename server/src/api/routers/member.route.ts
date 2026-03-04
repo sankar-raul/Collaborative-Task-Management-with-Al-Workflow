2d52fc32b5db24b13e4a3000428f745d9c4b2e95
sankar-raul <raulsankar99@gmail.com>
2026-03-04 15:45:17 +0530

    feat: add member retrieval and search functionalities in member service and controller

--- server/src/api/routers/member.route.ts
+++ server/src/api/routers/member.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllUsers, getAsignedProjects, getUserProjectRole } from "../controllers/member.controller"
+import { getAllUsers, getAsignedProjects, getUserProjectRole, getMemberById, getMyDetails, searchMembers } from "../controllers/member.controller"
 import adminAuth from "../middleware/adminAuth";
 
 const memberRoute = Router()
@@ -7,5 +7,9 @@ const memberRoute = Router()
 memberRoute.get("/projects", getAsignedProjects) // get all projects assigned to the user
 memberRoute.get("/projects/:id/role", getUserProjectRole) // get the role of the user in a specific project
 memberRoute.get("/all", adminAuth, getAllUsers) // get all users in the system with pagination
+memberRoute.get("/me", getMyDetails) // get the details of the logged in user
+memberRoute.get("/search", searchMembers) // search for users by name or email with pagination
+memberRoute.get("/:id", getMemberById) // get the details of a specific user by id
+
 
 export default memberRoute;
